d82b7d9e2da2e1d2dc26dcf26f9463daf1cfb902
sauron <dp20007@ues.edu.sv>
2025-10-29 21:37:54 -0600

    error en entidad proveedor

--- src/main/java/sv/edu/ues/occ/ingenieria/prn335/inventario/web/core/entity/Proveedor.java
+++ src/main/java/sv/edu/ues/occ/ingenieria/prn335/inventario/web/core/entity/Proveedor.java
@@ -2,38 +2,46 @@ package sv.edu.ues.occ.ingenieria.prn335.inventario.web.core.entity;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Size;
-import java.util.UUID;
 
 @Entity
 @Table(name = "proveedor", schema = "public")
 @NamedQueries({
-        @NamedQuery(name="Proveedor.findByIdProveedor", query = "SELECT p FROM Proveedor p WHERE p.id = :idProveedor"),
-        @NamedQuery(name="Proveedor.findAllProveedor", query = "SELECT p FROM Proveedor p"),
-        @NamedQuery(name="Proveedor.findByActivo", query = "SELECT p FROM Proveedor p WHERE p.activo = :activo"),
-        @NamedQuery(name="Proveedor.countAllProveedor", query = "SELECT COUNT(p) FROM Proveedor p"),
-        @NamedQuery(name="Proveedor.countByActivo", query = "SELECT COUNT(p) FROM Proveedor p WHERE p.activo = :activo"),
-        @NamedQuery(name="Proveedor.buscarProveedorPorNombre", query = "SELECT p FROM Proveedor p WHERE upper(p.nombre) LIKE :nombre")
+        @NamedQuery(name = "Proveedor.findByIdProveedor", query = "SELECT p FROM Proveedor p WHERE p.id = :idProveedor"),
+        @NamedQuery(name = "Proveedor.findAllProveedor", query = "SELECT p FROM Proveedor p"),
+        @NamedQuery(name = "Proveedor.findByActivo", query = "SELECT p FROM Proveedor p WHERE p.activo = :activo"),
+        @NamedQuery(name = "Proveedor.countByIdProveedor", query = "SELECT COUNT(p) FROM Proveedor p WHERE p.id = :idProveedor"),
+        @NamedQuery(name = "Proveedor.countAllProveedor", query = "SELECT COUNT(p) FROM Proveedor p"),
+        @NamedQuery(name = "Proveedor.countByActivo", query = "SELECT COUNT(p) FROM Proveedor p WHERE p.activo = :activo")
 })
 public class Proveedor {
-
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_proveedor", nullable = false)
-    private UUID id;
+    private Integer id;
 
     @Size(max = 155)
     @Column(name = "nombre", length = 155)
     private String nombre;
 
+    @Size(max = 155)
+    @Column(name = "razon_social", length = 155)
+    private String razonSocial;
+
+    @Size(max = 14)
+    @Column(name = "nit", length = 14)
+    private String nit;
+
     @Column(name = "activo")
     private Boolean activo;
 
-    // ========= GETTERS & SETTERS ==========
+    @Column(name = "observaciones", length = Integer.MAX_VALUE)
+    private String observaciones;
 
-    public UUID getId() {
+    public Integer getId() {
         return id;
     }
 
-    public void setId(UUID id) {
+    public void setId(Integer id) {
         this.id = id;
     }
 
@@ -45,6 +53,22 @@ public class Proveedor {
         this.nombre = nombre;
     }
 
+    public String getRazonSocial() {
+        return razonSocial;
+    }
+
+    public void setRazonSocial(String razonSocial) {
+        this.razonSocial = razonSocial;
+    }
+
+    public String getNit() {
+        return nit;
+    }
+
+    public void setNit(String nit) {
+        this.nit = nit;
+    }
+
     public Boolean getActivo() {
         return activo;
     }
@@ -52,4 +76,13 @@ public class Proveedor {
     public void setActivo(Boolean activo) {
         this.activo = activo;
     }
+
+    public String getObservaciones() {
+        return observaciones;
+    }
+
+    public void setObservaciones(String observaciones) {
+        this.observaciones = observaciones;
+    }
+
 }
